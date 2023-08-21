b3310555091fe4df9f97afb0d4a96a00369ce819
Kha Vy <vytrannguyen3004@gmail.com>
2023-08-21 15:42:35 +0700

    finish 404 page

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,14 +1,23 @@
 import { Button, Typography } from "@material-tailwind/react";
+import { useNavigate } from "react-router-dom";
 
 const NotFoundPage = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="relative">
-      <img src="/404.gif" alt="gif" className="border" />
+      <img src="/404.gif" alt="gif" />
 
-      <div className="absolute top-0 font-bold">
-        <Typography className="text-6xl">Page Not Found</Typography>
-        <Typography className="text-3xl">Oh No! Error 404</Typography>
-        <Button>Back to home page</Button>
+      <div className="absolute w-full top-0 text-center backdrop-blur-[1px]">
+        <Typography className="text-xl md:text-3xl font-bold">
+          Oh No! Error 404
+        </Typography>
+        <Typography className="md:text-xl font-bold">
+          Sorry we couldn't find the page you're looking for
+        </Typography>
+      </div>
+      <div className="absolute w-full bottom-0 flex justify-center">
+        <Button onClick={() => navigate("/")}>Back to home page</Button>
       </div>
     </div>
   );
