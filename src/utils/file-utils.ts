5bb2d9a5050d0415311c8da58999377e4384c845
Kha Vy <vytrannguyen3004@gmail.com>
2023-08-31 22:35:20 +0700

    add 2 function fileToBase64 and base64ToFile

---
+++ src/utils/file-utils.ts
@@ -0,0 +1,27 @@
+export const base64ToFile = (base64: string): File => {
+  const arr = base64.split(",");
+  const mimeTypeMatch = arr[0].match(/:(.*?);/);
+  const mimeType = mimeTypeMatch ? mimeTypeMatch[1] : "";
+  const bstr = atob(arr[1]);
+  let n = bstr.length;
+  const u8arr = new Uint8Array(n);
+
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+
+  return new File([u8arr], "image.png", { type: mimeType });
+};
+
+export const fileToBase64 = (
+  file: File
+): Promise<string | ArrayBuffer | null> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      resolve(reader.result);
+    };
+    reader.onerror = reject;
+    reader.readAsDataURL(file);
+  });
+};
