f2847e55390f99c444f97052411e3eafa3798cdf
Kha Vy <vytrannguyen3004@gmail.com>
2023-09-02 23:27:33 +0700

    update search-input

--- src/components/common/search-input.tsx
+++ src/components/common/search-input.tsx
@@ -1,13 +1,42 @@
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
-import { Button, Input } from "@material-tailwind/react";
+import { useDispatch, useSelector } from "react-redux";
+import { Button } from "@material-tailwind/react";
+import {
+  ArrowSmallLeftIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/solid";
+
+import { selectApp, setIsSearch } from "../../redux/features/app-state-slice";
 
 const SearchInput = () => {
+  const { isSearch } = useSelector(selectApp);
+
+  const dispatch = useDispatch();
+
   return (
-    <div className="relative flex w-full gap-2">
-      <Input type="search" label="Search" className="pr-20" crossOrigin={""} />
-      <Button size="sm" className="!absolute right-1 top-1 rounded">
-        <MagnifyingGlassIcon className="h-4 w-4" />
+    <div
+      className={`p-0.5 border-2 border-black rounded-full flex ${
+        isSearch && "w-full"
+      }`}
+    >
+      <Button
+        size="sm"
+        className="rounded-full normal-case flex items-center gap-2"
+        onClick={() => dispatch(setIsSearch(!isSearch))}
+      >
+        {!isSearch ? (
+          <>
+            <MagnifyingGlassIcon className="h-4 w-4" /> Search
+          </>
+        ) : (
+          <ArrowSmallLeftIcon className="h-4 w-4" />
+        )}
       </Button>
+      {isSearch && (
+        <input
+          className="bg-transparent w-full font-bold rounded-full px-3 focus:outline-0"
+          type="search"
+        />
+      )}
     </div>
   );
 };
