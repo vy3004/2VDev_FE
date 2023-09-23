51520da1f6ef29c7fd2e676c388f04e7f21a0867
Kha Vy <vytrannguyen3004@gmail.com>
2023-09-23 20:44:58 +0700

    add emun

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,1 +1,28 @@
 export const USER_LEVELS = ["Bronze", "Silver", "Gold", "Platinum", "Diamond"];
+
+export enum UserVerifyStatus {
+  Unverified, // chưa xác thực email, mặc định = 0
+  Verified, // đã xác thực email
+  Banned, // bị khóa
+}
+
+export enum UserRoleStatus {
+  User, // tào khoản người dùng, mặc định = 0
+  Admin, // tài khoản admin
+}
+
+export enum MediaType {
+  Image,
+  Video,
+}
+
+export enum QuestionType {
+  Post,
+  RePost,
+  Comment,
+}
+
+export enum ResolvedType {
+  unResolved,
+  Resolved,
+}
