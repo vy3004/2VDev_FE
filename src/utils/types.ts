171f259d75031a3860d37264ba200b0b7402e652
Kha Vy <vytrannguyen3004@gmail.com>
2023-09-23 20:43:26 +0700

    update interface Media, Tag, Post

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,5 @@
+import { MediaType, QuestionType, ResolvedType } from "./constant";
+
 export interface User {
   _id: string;
   username: string;
@@ -7,10 +9,43 @@ export interface User {
   verify: number;
   role: number;
   level: number;
-  point?: number;
+  point: number;
   bio?: string;
   location?: string;
   website?: string;
   avatar?: string;
   cover_photo?: string;
+}
+
+export interface Media {
+  url: string;
+  type: MediaType; // 0 = image, 1 = video
+}
+
+export interface Tag {
+  _id: string;
+  name: string;
+}
+
+export interface Post {
+  _id: string;
+  parent_id: null | string;
+  user_detail: User[];
+  title: string;
+  content: string;
+  medias: Media[];
+  type: QuestionType;
+  hashtags: Tag[];
+  resolved?: ResolvedType;
+  votes_count: number;
+  comment_count: number;
+  repost_count: number;
+  reports_count: number;
+  bookmarks_count: number;
+  views_count: number;
+  guest_views?: number;
+  user_views?: number;
+  reply_comment?: Post[];
+  created_at: string;
+  updated_at: string;
 }
