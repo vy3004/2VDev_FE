673ea6a11b5b6e80a0f6a087ac6bd463b35f5525
Kha Vy <vytrannguyen3004@gmail.com>
2023-09-23 20:44:27 +0700

    add func formatTime and formatTimeDistanceToNow

--- src/utils/string-utils.ts
+++ src/utils/string-utils.ts
@@ -1,3 +1,6 @@
+import { parseISO, formatDistanceToNow, format } from "date-fns";
+import { enUS, vi } from "date-fns/locale";
+
 export const getLastTwoWords = (str: string): string => {
   const words = str.split(" ");
   const lastTwoWords = words.slice(-2).join(" ");
@@ -8,11 +11,33 @@ export const splitPath = (path: string): { label: string; path: string }[] => {
   const parts = path.split("/").filter(Boolean);
   let currentPath = "";
 
-  return parts.map((part, index) => {
+  return parts.map((part) => {
     currentPath += `/${part}`;
     return {
       label: part,
       path: currentPath,
     };
   });
+};
+
+export const formatTimeDistanceToNow = (
+  time: string,
+  language: string
+): string => {
+  const parsedTime = parseISO(time);
+  const formattedTime = formatDistanceToNow(parsedTime, {
+    addSuffix: true,
+    locale: language === "en" ? enUS : vi,
+  });
+
+  return formattedTime;
+};
+
+export const formatTime = (time: string, language: string): string => {
+  const parsedTime = parseISO(time);
+  const formattedTime = format(parsedTime, "HH:mm EEEE, MMMM dd, yyyy", {
+    locale: language === "en" ? enUS : vi,
+  });
+
+  return formattedTime;
 };
