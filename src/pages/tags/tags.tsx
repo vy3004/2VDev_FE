13eb534fd26bc49a1072d0dd071efe2a66490b83
Kha Vy <vytrannguyen3004@gmail.com>
2023-11-25 20:24:59 +0700

    translate, responsive ui, fix bug

--- src/pages/tags/tags.tsx
+++ src/pages/tags/tags.tsx
@@ -24,7 +24,7 @@ const Tags = () => {
       const { response } = await tagService.getTags();
 
       if (response) {
-        // setTags(response.data.data);
+        setTags(response.data.data);
       }
 
       setIsLoading(false);
